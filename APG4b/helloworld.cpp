530b64f62a35deaa75cee28c54aac7f9453e1a13
uai <noema01@outlook.jp>
2021-04-16 07:10:21 +0900

    return atcorder

---
+++ APG4b/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout << "Hello world!" << endl;
+  cout << "a";
+  cout << "b" << endl;
+  cout << "c" << endl;
+  cout << 2038 << endl;
+}
