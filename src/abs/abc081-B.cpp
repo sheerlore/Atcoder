e2528db7951b58412d88c56a331b75ba160b4080
uai <noema01@outlook.jp>
2021-04-19 17:16:29 +0900

    train

---
+++ src/abs/abc081-B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+vector<int> A(210); 
+  
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++ ) cin >> A.at(i);
+
+  int res = 0;
+
+  
+
+
+
+  return 0;
+}
