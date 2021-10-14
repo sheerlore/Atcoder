f842004db8d3a01c5efb9b1d74db502d85566eb6
uai <noema01@outlook.jp>
2021-10-15 07:42:31 +0900

    フォルダの構造変更

---
+++ c++/abc/abc202-d.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+int main() {
+  int A, B, C;
+  // string ab[A+B];
+  string aa = "a";
+  string bb = "b";
+  cin >> A >> B >> C;
+  for (int i = 1; i < A; i++) aa += "a";
+  for (int i = 1; i < B; i++) bb += "b";
+
+
+  
+  return 0;
+}
